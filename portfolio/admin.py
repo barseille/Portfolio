57747e9e59a9288b48a3330d519d85fd07a5af2c
BarseillePonn <ponn.barseille@gmail.com>
2023-08-20 00:56:35 +0200

    création structure portfolio

---
+++ portfolio/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):    
+    list_display = ('id', 'username', 'email', 'is_superuser', 'is_staff', 'is_active')
+    list_filter = ('is_superuser', 'is_active')
+    
+admin.site.register(User, UserAdmin)
+
